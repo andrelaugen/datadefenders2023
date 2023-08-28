71338aa1d33940b8ee04215525efa3f0a97720a0
sreekanth.lakshminarasimha.murthy@dnb.no <sreekanth.lakshminarasimha.murthy@dnb.no>
2023-08-28 13:35:03 +0000

    New model companies_rated

--- models/consolidated/companies_rated.sql
+++ models/consolidated/companies_rated.sql
@@ -1,1 +1,17 @@
-select * from {{ ref('raw__company_identifier') }}
+select
+    pcs.reprisk_company_id,
+    pcs.company_name,
+    pcs.headquarters,
+    pcs.sectors,
+    pcs.company_lei,
+    mpcs.time_slot_name,
+    mpcs.current_rri,
+    mpcs.trend_rri,
+    mpcs.peak_rri,
+    mpcs.peak_rri_date,
+    mpcs.reprisk_rating
+from {{ ref("raw__company_identifier") }} pcs
+join
+    {{ ref("raw__company_matrices") }} mpcs
+    on pcs.reprisk_company_id = mpcs.reprisk_company_id
+order by mpcs.time_slot_name
