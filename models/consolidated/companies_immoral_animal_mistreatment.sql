47836884b2b25166d9e3eb728be95ec4ea5341a7
sreekanth.lakshminarasimha.murthy@dnb.no <sreekanth.lakshminarasimha.murthy@dnb.no>
2023-08-29 08:07:38 +0000

    sdvdsf

--- models/consolidated/companies_immoral_animal_mistreatment.sql
+++ models/consolidated/companies_immoral_animal_mistreatment.sql
@@ -1,9 +1,9 @@
 select distinct
     ci.reprisk_company_id,
+    ci.INCIDENT_DATE
     cr.company_name,
     related_countries,
-    animal_mistreatment,
-    child_labor
+    animal_mistreatment
 from {{ ref("raw__company_incidents") }} ci
 join {{ ref("companies_rated") }} cr on cr.reprisk_company_id = ci.reprisk_company_id
-where animal_mistreatment = true or child_labor = true
+where animal_mistreatment = true
---
+++ models/consolidated/companies_immoral_child_labour.sql
@@ -0,0 +1,9 @@
+select distinct
+    ci.reprisk_company_id,
+    ci.INCIDENT_DATE
+    cr.company_name,
+    related_countries,
+    child_labor
+from {{ ref("raw__company_incidents") }} ci
+join {{ ref("companies_rated") }} cr on cr.reprisk_company_id = ci.reprisk_company_id
+where child_labor = true
